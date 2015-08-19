3905ab6baafc91b2999df84f794ce7d96a57fbe1
charifmahmoudi <charif.mahmoudi@sfr.fr>
2015-08-19 06:49:20 +0200

    Add install Scripts

---
+++ InstallMasterAndSlave.sh
@@ -0,0 +1,10 @@
+cd /etc/apt/sources.list.d
+sudo wget http://public-repo-1.hortonworks.com/ambari/ubuntu12/2.x/updates/2.0.1/ambari.list
+mcc
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com B9733A7A07513CAD
+sudo apt-get update
+sudo apt-get install ambari-server
+sudo ambari-server setup
+sudo ambari-server start
+sudo apt-get install ntp
+sudo apt-get install ambari-agent
---
+++ installSlave.sh
@@ -0,0 +1,7 @@
+cd /etc/apt/sources.list.d
+sudo wget http://public-repo-1.hortonworks.com/ambari/ubuntu12/2.x/updates/2.0.1/ambari.list
+mcc
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com B9733A7A07513CAD
+sudo apt-get update
+sudo apt-get install ntp
+sudo apt-get install ambari-agent
